e1a0901139aa56e660891501f328a951b6d35419
MyStraw <kikongdosa@gmail.com>
2023-04-09 23:45:03 +0900

    연습추가

---
+++ src/practice/_01_matArray.java
@@ -0,0 +1,29 @@
+package practice;
+
+public class _01_matArray {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6 };
+		int n = 2;
+
+		int[][] mat = convertArrayToMatrix(arr, n);
+
+		// 변환된 2차원 배열 출력
+		for (int i = 0; i < n * 2 - 1; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static int[][] convertArrayToMatrix(int[] arr, int n) {
+		int[][] mat = new int[n * 2 - 1][n];
+		for (int i = 0; i < n * 2 - 1; i++) {
+			for (int j = 0; j < n; j++) {
+				mat[i][j] = arr[n * i + j];
+			}
+		}
+		return mat;
+	}
+}
