c9d813b5ed9d6d86783469366e827ae6e1a57a53
MyStraw <kikongdosa@gmail.com>
2023-04-13 23:58:16 +0900

    냐하하

---
+++ src/_doodle/Do2.java
@@ -0,0 +1,36 @@
+package _doodle;
+
+import java.util.Scanner;
+
+public class Do2 {
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a  = sc.nextInt();
+		int b = sc.nextInt();
+		
+		
+		
+		
+		double div = a/b;
+		
+		
+		System.out.println(div);
+		
+		
+		
+		
+				
+		
+		
+		
+
+		
+		
+		
+	}
+
+}
